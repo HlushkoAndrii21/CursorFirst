bd58cbe100fb71318b0e7d3d794687c89c29c93e
HlushkoAndrii <hlushkoandrii21@ukr.net>
2021-12-16 23:32:25 +0200

    FirstHomeWork

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        char a = 123;
-        System.out.println(a);
+        double x = 2;
+        double y = 4;
+        double a = 6;
+        double b = 8;
+        double w = 10;
+        double X = 5;
+        double e = 3;
+        double result = x+y-a/2*(x+w);
+        System.out.println(result);
+        double secondResult = a*(x+x)/y-a*3;
+        System.out.println(secondResult);
+        double thirdResult = a+b/x-y;
+        System.out.println(thirdResult);
+        double fourthResult = X-e/2%e+1;
+        System.out.println(fourthResult);
         // git init - ініціалізація git репозиторію
         // git add . - додати всы файли в git
         // git commit -m "Message" - зробити коміт (занотувати зміни)
