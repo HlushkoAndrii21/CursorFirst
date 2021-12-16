551d14e285de6b3cf6cca2d940c6ecc4665f64d5
HlushkoAndrii <hlushkoandrii21@ukr.net>
2021-12-16 21:53:53 +0200

    addet git tips

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         char a = 123;
         System.out.println(a);
+        // git init - ініціалізація git репозиторію
+        // git add . - додати всы файли в git
+        // git commit -m "Message" - зробити коміт (занотувати зміни)
     }
 }
